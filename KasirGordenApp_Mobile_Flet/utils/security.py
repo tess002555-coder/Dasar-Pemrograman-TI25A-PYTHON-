175df32b2d6b3fc9cce3a98735f8e163bfe6b8f2
tess002555-coder <tess002555@gmail.com>
2026-07-15 22:20:51 +0700

    Delete KasirGordenApp_Mobile_Flet directory

--- KasirGordenApp_Mobile_Flet/utils/security.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def verify_password(stored_password_hash, provided_password):
-    return stored_password_hash == hash_password(provided_password)
--- KasirGordenApp_Mobile_Flet/views_mobile/__init__.py
+++
@@ -1,1 +0,0 @@
-# views_mobile package
